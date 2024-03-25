b63128ea92452af9da1c01bc2eda4b70b2039c70
tanjaabdulova@yandex.ru <tanjaabdulova@yandex.ru>
2024-03-25 10:49:19 +0500

    2.1

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -4,13 +4,18 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class ChatServer {
+    private static final int DEFAULT_PORT = 1234;
     ServerSocket serverSocket;
     ArrayList<Client> clients = new ArrayList<>();
-    // создаем серверный сокет на порту 1234
-
 
     ChatServer() throws IOException {
-        serverSocket = new ServerSocket(1234);
+        serverSocket = new ServerSocket(DEFAULT_PORT);
+    }
+
+    void sendAll(String message) {
+        for (Client client : clients) {
+            client.receive(message);
+        }
     }
 
     public void run() {
@@ -20,7 +25,7 @@ public class ChatServer {
                 // ждем клиента из сети
                 Socket socket = serverSocket.accept();
                 System.out.println("Client connected!");
-                clients.add(new Client(socket));
+                clients.add(new Client(socket, this));
             } catch (IOException e) {
                 e.printStackTrace();
             }
